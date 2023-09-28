d634a6e30a7c7c77065826d61c035bddd20603dd
Quartezz <kamil.machel03@gmail.com>
2023-09-28 12:20:07 +0200

    created app function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
+export default function App() {
   return (
     <>
-      
+      <Header />
+      <Footer />
     </>
   )
 }
 
-export default App

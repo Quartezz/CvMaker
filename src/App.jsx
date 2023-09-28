0b4bf9cd970c7a7009d20b0fb19fa4f72c99b3f6
Quartezz <kamil.machel03@gmail.com>
2023-09-28 12:23:30 +0200

    deleted import use state from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Main from "./components/Main"
 
 export default function App() {
   return (
     <>
       <Header />
+      <Main />
       <Footer />
     </>
   )

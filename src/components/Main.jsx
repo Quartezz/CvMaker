c5071b153c5559b46433201d7630786b0ecdb2a5
Quartezz <kamil.machel03@gmail.com>
2023-09-28 14:09:00 +0200

    added functions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -41,4 +41,100 @@ export default function Main() {
         setPhoneNumber(event.target.value)
     }
 
+    function clear(e) {
+        for (let i = 0; i < e.target.length; i++) {
+            e.target[i].value = ""
+        }
+    }
+
+    function addExperience(e) {
+        e.preventDefault()
+        const id = uuidv4()
+        const position = e.target[0].value
+        const company = e.target[1].value
+        const start = e.target[2].value
+        const end = e.target[3].value
+        const desc = e.target[4].value
+
+        let newExperience = { id, position, company, start, end, desc}
+        setExperience([...experience, newExperience])
+        clear(e)
+    }
+
+    function deleteExperience(id) {
+        setExperience((currentExperience) => {
+            return currentExperience.filter((exp) => exp.id !== id)
+        })
+    }
+
+    function saveExperience(e, id) {
+        e.preventDefault()
+
+        let position = e.target[0].value
+        let company = e.target[1].value
+        let start = e.target[2].value
+        let end = e.target[3].value
+        let desc = e.target[4].value
+        let updateExp = { position, company, start, end, desc }
+        setExperience(
+            experience.map((exp) => {
+                if (exp.id === id) {
+                    return { ...exp, ...updateExp}
+                } else {
+                    return exp
+                }
+            })
+        )
+    }
+
+    function addEducation(e) {
+        e.preventDefault()
+        const id = uuidv4()
+        const course = e.target[0].value
+        const university = e.target[1].value
+        const start = e.target[2].value
+        const end = e.target[3].value
+        let newEducation = { id, course, university, start, end }
+
+        setEducation([...education, newEducation])
+        clear(e)
+    }
+
+    function saveEducation(e, id) {
+        e.preventDefault();
+    
+        const course = e.target[0].value;
+        const university = e.target[1].value;
+        const start = e.target[2].value;
+        const end = e.target[3].value;
+        const updateEdu = { course, university, start, end };
+        setEducation(
+          education.map((edu) => {
+            if (edu.id === id) {
+              return { ...edu, ...updateEdu };
+            } else {
+              return edu;
+            }
+          })
+        );
+      }
+
+      function deleteEducation(id) {
+        setEducation((currentEducation) => {
+          return currentEducation.filter((edu) => edu.id !== id);
+        });
+      }
+
+      function reset() {
+        const empty = []
+        setEducation(empty)
+        setExperience(empty)
+        setFirstName("");
+        setLastName("");
+        setTitle("");
+        setEmail("");
+        setAddress("");
+        setPhoneNumber("");
+      }
+
 }

dad10b02dec8ec0f2ad020d7259c9635c30fcfcf
Quartezz <kamil.machel03@gmail.com>
2023-09-28 12:39:44 +0200

    created form for personal info

---
+++ src/components/Form/PersonalInfo.jsx
@@ -0,0 +1,27 @@
+import React, {Component, useState} from "react";
+
+export default function PersonalInfo(
+    {
+        FirstName,
+        LastName,
+        Address,
+        Email,
+        Description,
+        Title,
+        Phone,
+    }
+) {
+    return (
+        <>
+        <div id="personal-info">
+            <input type="text" placeholder="First Name" onChange={FirstName} />
+            <input type="text" placeholder="Last Name" onChange={LastName} />
+            <input type="text" placeholder="Title" onChange={Title} />
+            <input type="text" placeholder="Adress" onChange={Address} />
+            <input type="text" placeholder="Phone Number" onChange={Phone} />
+            <input type="text" placeholder="Email" onChange={Email} />
+            <textarea placeholder="Description" className="description" onChange={Description}></textarea>
+        </div>
+        </>
+    )
+}
